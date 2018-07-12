7335bebb9ff0bb548baaed180bdf0d8f37fe76f9
aokomoriuta <internal@enmps.net>
2018-07-12 19:49:23 +0900

    +time

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <memory>
 #include <random>
+#include <chrono>
 
 #include <cblas.h>
 
 // C = AB
 int main()
 {
-	constexpr auto N = std::size_t(10000);
-	constexpr auto M = std::size_t(1000);
-	constexpr auto L = std::size_t(100);
+	constexpr auto N = std::size_t(100000);
+	constexpr auto M = std::size_t(10000);
+	constexpr auto L = std::size_t(1000);
 
 	constexpr auto N_A = N;
 	constexpr auto M_A = L;
@@ -40,10 +41,18 @@ int main()
 	constexpr auto TRANS_B = ::CblasNoTrans;
 	constexpr auto ALPHA = double(1);
 	constexpr auto BETA = double(0);
+
+	std::cout << N_A << "x" << M_A << " * " << N_B << "x" << M_B << std::endl;
+	const auto begin = std::chrono::system_clock::now();
 	::cblas_dgemm(ORDER, TRANS_A, TRANS_B, N, M, L, ALPHA,
 		A.get(), M_A,
 		B.get(), M_B, BETA,
 		C.get(), M_C);
+	const auto end = std::chrono::system_clock::now();
+	const auto us = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+	const auto gflops = N*(2*M - 1)*L / us * 1e-3;
+	std::cout << us*1e-3 << "[ms]" << std::endl;
+	std::cout << gflops << "[GFLOP/s]" << std::endl;
 
 	return 0;
 }
