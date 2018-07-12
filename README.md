722d64809b04c399f81469053caed5967d90eda4
aokomoriuta <internal@enmps.net>
2018-07-12 19:37:44 +0900

    dgemm

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ You can select the license of your choice from as following:
 * [Creative Commons Attribution-ShareAlike 3.0 Unported](http://creativecommons.org/licenses/by-sa/3.0/)
 * [Creative Commons Attribution-Noncommercial 3.0 Unported](http://creativecommons.org/licenses/by-nc/3.0/)
 * [GNU General Public License v3.0 or later](http://www.gnu.org/licenses/gpl.html)
+
+## Usage
+
+```shell
+$ sudo apt install libopenblas-dev
+$ cd src
+$ make
+$ ./cblas_test
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <memory>
+#include <random>
 
+#include <cblas.h>
+
+// C = AB
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	constexpr auto N = std::size_t(10000);
+	constexpr auto M = std::size_t(1000);
+	constexpr auto L = std::size_t(100);
+
+	constexpr auto N_A = N;
+	constexpr auto M_A = L;
+	constexpr auto N_B = L;
+	constexpr auto M_B = M;
+	constexpr auto N_C = N;
+	constexpr auto M_C = M;
+
+	auto A = std::make_unique<double[]>(N_A * M_A);
+	auto B = std::make_unique<double[]>(N_B * M_B);
+	auto C = std::make_unique<double[]>(N_C * M_C);
+
+	auto generator = std::mt19937(12345);
+	auto distribution = std::uniform_real_distribution<double>(-10, 10);
+
+	for(auto i = decltype(N_A*M_A)(0); i < N_A*M_A; ++i)
+	{
+		A[i] = distribution(generator);
+	}
+	for(auto i = decltype(N_B*M_B)(0); i < N_B*M_B; ++i)
+	{
+		B[i] = distribution(generator);
+	}
+	std::fill_n(C.get(), N_C*M_C, 0);
+
+	constexpr auto ORDER = ::CblasRowMajor;
+	constexpr auto TRANS_A = ::CblasNoTrans;
+	constexpr auto TRANS_B = ::CblasNoTrans;
+	constexpr auto ALPHA = double(1);
+	constexpr auto BETA = double(0);
+	::cblas_dgemm(ORDER, TRANS_A, TRANS_B, N, M, L, ALPHA,
+		A.get(), M_A,
+		B.get(), M_B, BETA,
+		C.get(), M_C);
+
 	return 0;
 }
